edca530d2f595c465f416a4ff913cc4f7d316f44
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-02-01 16:35:37 +0600

    Brand List Fixed!

--- lib/models/brand.dart
+++ lib/models/brand.dart
@@ -3,17 +3,5 @@ class Brand {
   String pk;
 
   Brand(this.name, this.pk);
-
-  // Brand(Map<String, dynamic> data) {
-  //   name = data['fields']['name'];
-  //   pk = data['pk'].toString();
-  // }
-
-  // factory Brand.fromJson(Map<String, dynamic> parsedJson){
-  //   return Brand(
-  //     name: parsedJson['fields']['name'],
-  //     pk : parsedJson['pk'].toString(),
-  //   );
-  // }
-
+  
 }
