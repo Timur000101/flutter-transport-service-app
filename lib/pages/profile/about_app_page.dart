467095e79ef4e7e8ca7366004bbe0f6edb37a4c6
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-02-10 15:37:03 +0600

    Added functional for Support and About App Pages.

--- lib/pages/profile/about_app_page.dart
+++ lib/pages/profile/about_app_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:sto_app/core/const.dart';
 import 'package:sto_app/widgets/app_widgets.dart';
 
+import 'terms_of_use_page.dart';
+
 class AboutAppPage extends StatefulWidget {
   @override
   _AboutAppPageState createState() => _AboutAppPageState();
@@ -13,6 +15,52 @@ class _AboutAppPageState extends State<AboutAppPage> {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       appBar: buildAppBar("О приложении"),
+      body: Container(
+        color: AppColors.backgroundColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.asset(
+              "assets/images/logo_FS_color.jpg",
+              width: MediaQuery.of(context).size.width - 100,
+            ),
+          ),
+          Text('Мобильное приложение - Bumper.kz', style: TextStyle(color: AppColors.primaryTextColor, fontSize: 14)),
+          SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 1,
+              itemBuilder: (_, index) {
+                return Container(
+                  margin: EdgeInsets.only(bottom: 2.5),
+                  child: ListTile(
+                    title: Text('Пользовательское соглашение', style: TextStyle(fontSize: 14)),
+                    leading: Icon(Icons.warning, color: AppColors.mainColor, size: 30,),
+                    trailing: Icon(Icons.keyboard_arrow_right_outlined),
+                    tileColor: Colors.white,
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => TermsOfUsePage()),
+                      );
+                    },
+                  ),
+                );
+              }
+            ),
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text('Спасибо что выбрали нас!', style: TextStyle(color: AppColors.primaryTextColor, fontSize: 14)),
+          ),
+        ],),
+      ),
     );
   }
 }
--- lib/pages/profile/support_page.dart
+++ lib/pages/profile/support_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sto_app/core/const.dart';
 import 'package:sto_app/widgets/app_widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io' show Platform;
 
 class SupportPage extends StatefulWidget {
   @override
@@ -13,7 +15,248 @@ class _SupportPageState extends State<SupportPage> {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       appBar: buildAppBar("Служба поддержки"),
+      body: Container(
+        color: AppColors.backgroundColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.asset(
+              "assets/images/logo_FS_color.jpg",
+              width: MediaQuery.of(context).size.width - 100,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+            child: Text('Рады помочь! Служба поддержки Bumper.kz', style: TextStyle(color: AppColors.primaryTextColor, fontSize: 14)),
+          ),
+          GestureDetector(
+            onTap: (){
+              call();
+            },
+            child:Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              decoration: BoxDecoration(
+                  color: AppColors.lightColor,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12.withOpacity(0.1),
+                        spreadRadius: 3,
+                        blurRadius: 7,
+                        offset: Offset(0, 1))
+                  ]),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.call, color:AppColors.mainColor ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 5),
+                        child: Text(
+                            'Позвонить',
+                            style: TextStyle(color: AppColors.mainTextColor,fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Text(
+                          '+7(778)357-92-79',
+                          style: TextStyle(color: AppColors.mainTextColor,fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: AppColors.lightColor,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12.withOpacity(0.1),
+                              spreadRadius: 3,
+                              blurRadius: 7,
+                              offset: Offset(0, 1))
+                        ]),
+                    child: Icon(Icons.arrow_forward_ios_rounded)
+                  ),
+                ],
+              ),
+            )
+          ),
+          GestureDetector(
+            onTap: (){
+              message();
+            },
+            child:Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              decoration: BoxDecoration(
+                  color: AppColors.lightColor,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12.withOpacity(0.1),
+                        spreadRadius: 3,
+                        blurRadius: 7,
+                        offset: Offset(0, 1))
+                  ]),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.email, color:AppColors.mainColor ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 5),
+                        child: Text(
+                            'Написать',
+                            style: TextStyle(color: AppColors.mainTextColor,fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: SizedBox(
+                          child: Text(
+                            'support@bumper.kz',
+                            style: TextStyle(color: AppColors.mainTextColor,fontSize: 14),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: AppColors.lightColor,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12.withOpacity(0.1),
+                              spreadRadius: 3,
+                              blurRadius: 7,
+                              offset: Offset(0, 1))
+                        ]),
+                    child: Icon(Icons.arrow_forward_ios_rounded)
+                  ),
+                ],
+              ),
+            )
+          ),
+          GestureDetector(
+            onTap: (){
+              visitSite();
+            },
+            child:Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              decoration: BoxDecoration(
+                  color: AppColors.lightColor,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12.withOpacity(0.1),
+                        spreadRadius: 3,
+                        blurRadius: 7,
+                        offset: Offset(0, 1))
+                  ]),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.message, color:AppColors.mainColor ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 5),
+                        child: Text(
+                            'Посетить сайт',
+                            style: TextStyle(color: AppColors.mainTextColor,fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: SizedBox(
+                          child: Text(
+                            'Bumper.kz',
+                            style: TextStyle(color: AppColors.mainTextColor,fontSize: 14),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: AppColors.lightColor,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12.withOpacity(0.1),
+                              spreadRadius: 3,
+                              blurRadius: 7,
+                              offset: Offset(0, 1))
+                        ]),
+                    child: Icon(Icons.arrow_forward_ios_rounded)
+                  ),
+                ],
+              ),
+            )
+          )
+        ],),
+      ),
     );
   }
 
+  call(){
+    launch("tel://+77783579279");
+  }
+
+  message(){
+    final Uri _emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: 'support@bumper.kz',
+      queryParameters: {
+        'subject': 'Example Subject & Symbols are allowed!'
+      }
+    );
+    launch(_emailLaunchUri.toString());
+  }
+
+  visitSite() async {
+    var url = "";
+    if (Platform.isAndroid) {
+      url = "https://play.google.com/store/apps/";
+    }
+    if (Platform.isIOS) {
+      url = 'https://apple.com';
+    }
+    if (await canLaunch(url)) {
+      await launch(url);
+    } 
+    else {
+      throw 'Could not launch $url';
+    }
+  }
 }
