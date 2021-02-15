f637a9a1ab13c31c43039798f434d88c273126a1
sultan841 <seyitmat@mail.ru>
2021-02-15 19:05:27 +0600

    botom sheet view

--- lib/pages/order/order_active_item.dart
+++ lib/pages/order/order_active_item.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sto_app/core/const.dart';
 import 'package:sto_app/models/order_history.dart';
+import 'package:url_launcher/url_launcher.dart';
+// import "package:platform/platform.dart";
 
 class OrderActiveItem extends StatelessWidget {
   final OrderHistory orderHistory;
@@ -120,29 +123,122 @@ class OrderActiveItem extends StatelessWidget {
   String avaURL =
       "https://www.sunsetlearning.com/wp-content/uploads/2019/09/User-Icon-Grey.png";
 
+  Color getGreenColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return AppColors.greenColor;
+    }
+    return AppColors.greenColor;
+  }
+
+  Color getRedColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.red[600];
+    }
+    return Colors.red[600];
+  }
+
   void _settingModalBottomSheet(context) {
     showModalBottomSheet(
-        context: context,
-        builder: (BuildContext bc) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage(avaURL),
-                  radius: 45,
-                ),
+      context: context,
+      builder: (BuildContext bc) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(avaURL),
+                radius: 40,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  "Sherlock Holmes",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Text(
+                "Sherlock Holmes",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-              Text("+7781661021"),
-            ],
-          );
-        });
+            ),
+            Text("+7781661021",
+                style: TextStyle(color: AppColors.primaryTextColor)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        var phone = "87781661021";
+                        var message = "hi";
+                        urlMessage(message, phone);
+                      },
+                      icon: Icon(Icons.message),
+                      label: Text("Написать"),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.resolveWith(
+                              getGreenColor))),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        launch("tel://214324234");
+                      },
+                      icon: Icon(Icons.call),
+                      label: Text("Позвонить"),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.resolveWith(
+                              getGreenColor))),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Испонитель закончил ваш заказ, вы подтверждаете завершение?",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 30),
+                    child: Text(
+                      "Завершить заказ",
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.resolveWith(getRedColor),
+                  )),
+            )
+          ],
+        );
+      },
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+
+  String urlMessage(String message, String phone) {
+    if (Platform.isAndroid) {
+      // add the [https]
+      return "https://wa.me/$phone/?text=${Uri.parse(message)}"; // new line
+    } else {
+      // add the [https]
+      return "https://api.whatsapp.com/send?phone=$phone=${Uri.parse(message)}"; // new line
+    }
   }
 }
